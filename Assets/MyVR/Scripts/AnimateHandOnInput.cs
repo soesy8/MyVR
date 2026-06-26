c05632a173a7ae19230f14fa124446fd93b39310
SoEsy8 <soesy8@gmail.com>
2026-06-26 18:50:36 +0900

    헤드셋, 좌우 손 위치 인식 및 입력에 따른 애니메이션 구현

---
+++ Assets/MyVR/Scripts/AnimateHandOnInput.cs
@@ -0,0 +1,41 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+namespace MyVR
+{
+    /// <summary>
+    /// 인풋 받아서 손 애니메이션 구현
+    /// </summary>
+    public class AnimateHandOnInput : MonoBehaviour
+    {
+        #region Variables
+        //참조
+        private Animator animator;
+
+        //인풋
+        public InputActionProperty pinchAnimationAction;
+        public InputActionProperty gripAnimationAction;
+
+        //애니메이터 파라미터
+        private string grip = "Grip";
+        private string trigger = "Trigger";
+        #endregion
+
+        #region Unity Event Method
+        private void Awake()
+        {
+            animator = GetComponent<Animator>();
+        }
+
+        private void Update()
+        {
+            //인풋처리
+            float triggerValue = pinchAnimationAction.action.ReadValue<float>();
+            float gripValue = gripAnimationAction.action.ReadValue<float>();
+
+            animator.SetFloat(trigger, triggerValue);
+            animator.SetFloat(grip, gripValue);
+        }
+        #endregion
+    }
+}
